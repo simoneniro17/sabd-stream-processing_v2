98ea6fc1c96c5bf66fe2f72ecf5b4054b01f448f
simoneniro17 <simoneniro17@gmail.com>
2025-06-12 17:22:10 +0200

    Aggiunto test deserializer

---
+++ flink/src/main/java/it/flink/MsgPackDeserializationSchema.java
@@ -0,0 +1,29 @@
+package it.flink;
+
+import org.apache.flink.api.common.serialization.DeserializationSchema;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.msgpack.jackson.dataformat.MessagePackFactory;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class MsgPackDeserializationSchema implements DeserializationSchema<Map<String, Object>> {
+
+    private static final ObjectMapper mapper = new ObjectMapper(new MessagePackFactory());
+
+    @Override
+    public Map<String, Object> deserialize(byte[] message) throws IOException {
+        return mapper.readValue(message, Map.class);
+    }
+
+    @Override
+    public boolean isEndOfStream(Map<String, Object> nextElement) {
+        return false;
+    }
+
+    @Override
+    public TypeInformation<Map<String, Object>> getProducedType() {
+        return TypeInformation.of(new org.apache.flink.api.common.typeinfo.TypeHint<Map<String, Object>>() {});
+    }
+}
